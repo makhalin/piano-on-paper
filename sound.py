97cbd6a7575fe8ebfa2567cc3e18c2822f8c9137
gebetix <gebetix@gmail.com>
2013-10-15 18:00:12 +0400

    Добавил черные клавиши

--- sound.py
+++ sound.py
@@ -8,13 +8,15 @@ class SoundPlayer:
     "Class for playing piano notes"
     def __init__(self, filename):
         fluidsynth.init(filename)
-        note_names = ("C-4", "D-4", "E-4", "F-4", "G-4", "A-4", "B-4", "C-5", "D-5", "E-5", "F-5", "G-5", "A-5", "B-5")
-        coords = (15, 52, 88, 125, 160, 197, 234, 270, 307, 343, 378, 415, 452, 487)
-
+        note_names = ("C-4", "C#4", "D-4", "D#4", "E-4", "F-4", "F#4", "G-4", "G#4", "A-4", "A#4", "B-4",
+                      "C-5", "C#5", "D-5", "D#5", "E-5", "F-5", "F#5", "G-5", "G#5", "A-5", "A#5", "B-5")
+        coords = (15, 35, 52, 71, 88, 125, 144, 160, 180, 197, 216, 234, 
+                  270, 289, 307, 325, 343, 378, 398, 415, 434, 452, 470, 487)
+        
         self.is_pressed = dict.fromkeys(note_names, [False] * len(note_names))
         self.note_coordinate = dict(zip(note_names, coords))
         self.note_names = note_names
-        
+
         self.window_title = 'Keyboard'
         cv2.namedWindow(self.window_title)
         self.img = cv2.imread('./resources/keyboard.jpg', cv2.CV_LOAD_IMAGE_COLOR)
@@ -39,9 +41,14 @@ class SoundPlayer:
     def draw_piano(self):
         white = (255, 255, 255)
         red = (0, 0, 255)
+        black = (0, 0, 0)
         for note in self.is_pressed:
             color = red if self.is_pressed[note] else white
-            cv2.circle(self.img, (self.note_coordinate[note], 210), 10, color, -1)
+            if note[1:2] == '-':
+                cv2.circle(self.img, (self.note_coordinate[note], 210), 10, color, -1)
+            else:
+                color = red if self.is_pressed[note] else black
+                cv2.circle(self.img, (self.note_coordinate[note], 110), 10, color, -1)
 
         cv2.imshow(self.window_title, self.img)
 
@@ -50,5 +57,5 @@ class SoundPlayer:
 
     def turn_sound_off(self):
         self.is_sound_on = False
-        
+       
         
